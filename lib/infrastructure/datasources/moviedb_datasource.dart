4e6624119e8e6b24acbaf3971ef07b6afa92d6b6
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-09-01 12:46:57 -0500

    movie mapper finished

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -18,6 +18,7 @@ class MoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     
     final response = await dio.get('/movie/now_playing');
+    
     final List<Movie> movies = [];
 
 
