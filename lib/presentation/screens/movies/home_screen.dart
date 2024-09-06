87029881cfb8973b82e92641759f557722c1115b
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-09-06 17:40:48 -0500

    initial loading finished

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -39,14 +39,14 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
 
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final initialLoading = ref.watch( initialLoadingProvider );
+    if ( initialLoading ) return const FullScreenLoader();
+
     final slideShowMovies = ref.watch( moviesSlideshowProvider );
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
     final popularMovies = ref.watch( popularMoviesProvider );
     final upcomingMovies = ref.watch( upcomingMoviesProvider );
     final topRatedMovies = ref.watch( topRatedMoviesProvider );
-
-    return const FullScreenLoader();
-
 
     return CustomScrollView(
       slivers: [
