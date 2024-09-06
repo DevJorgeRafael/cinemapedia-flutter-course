676020ba2784d792c26767e3065fadcbd2dcfdf3
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-09-06 17:14:49 -0500

    Full Screen loader

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -45,6 +45,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     final upcomingMovies = ref.watch( upcomingMoviesProvider );
     final topRatedMovies = ref.watch( topRatedMoviesProvider );
 
+    return const FullScreenLoader();
+
 
     return CustomScrollView(
       slivers: [
